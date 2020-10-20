8dd295b293f6025d1fdcaaabd2baf51be62e5619
apalom <35828580+apalom@users.noreply.github.com>
2020-10-20 10:06:12 -0600

    Update entropy_and_information_gain.py

--- hw1/entropy_and_information_gain.py
+++ hw1/entropy_and_information_gain.py
@@ -12,6 +12,7 @@ import math
 print('--- Decision Trees Q2b ---')
 
 p = [5/9, 4/9]
+p = [1/2, 1/2]
 
 def entropy(p):
     H = 0
@@ -74,8 +75,43 @@ for f in features:
 
     print('Information Gain by GINI: {:.4f}'.format(Gg))
 
+#%% calculate information gain by ID3
+
+print('\n --- Decision Trees Q2c, Q3a, and Q3b ---')
+
+features = ['x1', 'x2','x3', 'x4']
+for f in features:        
+    S = 4;
+    print('\nFeature: ', f)
+    if f == 'x1':
+        A = [[2/3,1/3],[1/1]]
+        Sv = [3,1]
+
+    if f == 'x2':
+        A = [[2/2],[2/2]]
+        Sv = [2,2]
+
+    if f == 'x3':
+        A = [[1/2,1/2],[1/2,1/2]]
+        Sv = [2,2]
+
+    if f == 'x4':
+        A = [[1/3,2/3],[1/1]]
+        Sv = [3,1]
+
+    # calculate information gain by misclassification rate
+    for ai in A:
+        I = 0;
+        print(f, ai)
+        for i in range(len(ai)):
+            I += (Sv[i]/S) * entropy(ai)
+    G = (H - I)
+        
+    print('Information Gain by ID3: {:.4f}'.format(G))
 
 
+#%%
+3/4*(-2/3*np.log2(2/3)-1/3*np.log2(1/3))
 
 
 
